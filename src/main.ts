2d5ccc9489a7a1c958afc970416c0e814e43644a
Andrew Grabynsky <hrabynsky@gmail.com>
2024-10-11 14:36:46 +0300

    end lesson 6

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Preview example')
+    .setDescription('The preview API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModelsExpandDepth: 1,
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(3000);
 }
-bootstrap();
+
+void bootstrap();
---
+++ src/user/dto/res/user.response.dto.ts
@@ -0,0 +1,9 @@
+export class UserResponseDto {
+  id: string;
+
+  name?: string;
+
+  email: string;
+
+  password: string;
+}
